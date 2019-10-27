a2a33983c43f024cb34a36f6efb64afc639ff399
nguyenduysu <su.nguyenduy.nb@gmail.com>
2019-10-27 10:43:43 +0700

    update

--- ServerSide/ChessImpl.java
+++ ServerSide/ChessImpl.java
@@ -5,13 +5,34 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class ChessImpl extends UnicastRemoteObject implements ChessInterface{
 	
+	public int[] n = new int[4];
+	public String myType = "none"; //ne fillim asgje
 	public ChessImpl() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public void Test() throws RemoteException {
-		System.out.println("Hello World");
+	
+	public void sendMoveAndReciveServer(int mySX,int mySY,int myDesColumn, int myDesRow) 
+												throws RemoteException{
+		n[0] = mySX;
+		n[1] = mySY;
+		n[2] = myDesColumn;
+		n[3] = myDesRow;
+ 	}
+	
+	//Test
+	public int[] getMoveLocation() throws RemoteException{
+		return n;
+	}
+	
+	
+	
+	public void tellWhoHasToPlay(String s) throws RemoteException{
+		myType = s;
+	}
+	
+	public String getTellWhoHasToPlay() throws RemoteException{
+		return myType;
 	}
 
 }
